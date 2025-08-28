e8dbc2df317fb7c1236a7380786a299e20c3c31b
Saymon Felipe <80544520+saymon-felipe@users.noreply.github.com>
2025-08-27 23:02:32 -0300

    URL Api teste

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -3,16 +3,28 @@ import axios from 'axios';
 let url_api;
 
 const dev_environment = "http://localhost:3000/"; // Ambiente de desenvolvimento.
-const prod_environment = "https://core-ksi-0965169e8f49.herokuapp.com/"; // Ambiente de teste.
+const test_environment = "https://coretest-ksi-6126217aaaf1.herokuapp.com/"; // Ambiente de teste.
+const prod_environment = "https://core-ksi-0965169e8f49.herokuapp.com/"; // Ambiente de produção.
 
 // Detecta ambiente automaticamente
 let ambient = (window.location.hostname.includes("localhost") || window.location.hostname.includes("192.168")) ? 0 : 1;
+
+if (window.location.hostname.includes("localhost") || window.location.hostname.includes("192.168")) {
+  ambient = 0;
+} else if (window.location.hostname.includes("dev")) {
+  ambient = 1;
+} else {
+  ambient = 2;
+}
 
 switch (ambient) {
   case 0:
     url_api = dev_environment;
     break;
   case 1:
+    url_api = test_environment;
+    break;
+  case 2:
     url_api = prod_environment;
     break;
 }
