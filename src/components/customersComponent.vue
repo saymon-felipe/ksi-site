badbd1bdfd945c28e9147f2520ef9239cf8858ad
Saymon Felipe <80544520+saymon-felipe@users.noreply.github.com>
2025-09-04 00:34:34 -0300

    Otimização das imagens dos clientes

--- src/components/customersComponent.vue
+++ src/components/customersComponent.vue
@@ -16,85 +16,85 @@ export default {
         return {
             clientes: [
                 {
-                    imagem: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/agendaspro.png",
+                    imagem: "https://kineticsolutions.s3.sa-east-1.amazonaws.com/agendaspro_otimizada.webp",
                     nome: "AgendasPRO",
                     link: "https://agendaspro.com/",
                     icone: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/agendaspro-logo.png"
                 },
                 {
-                    imagem: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/agendaspro-sistema.png",
+                    imagem: "https://kineticsolutions.s3.sa-east-1.amazonaws.com/agendaspro-sistema_otimizada.webp",
                     nome: "Sistema AgendasPRO",
                     link: "https://dev.agendaspro.com/",
                     icone: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/agendaspro-logo.png"
                 },
                 {
-                    imagem: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/portal-aluno-thumb.png",
+                    imagem: "https://kineticsolutions.s3.sa-east-1.amazonaws.com/portal-aluno-thumb_otimizada.webp",
                     nome: "Solutto - Educacional",
                     icone: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/solutto-logo.png",
                     link: "https://solutto.com.br/segmento-franquia-educacional.aspx"
                 },
                 {
-                    imagem: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/portal-aluno-2-thumb.png",
+                    imagem: "https://kineticsolutions.s3.sa-east-1.amazonaws.com/portal-aluno-2-thumb_otimizada.webp",
                     nome: "Solutto - Educacional",
                     icone: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/solutto-logo.png",
                     link: "https://solutto.com.br/segmento-franquia-educacional.aspx"
                 },
                 {
-                    imagem: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/solutto-thumb.png",
+                    imagem: "https://kineticsolutions.s3.sa-east-1.amazonaws.com/solutto-thumb_otimizada.webp",
                     nome: "Solutto - ERP para franquias",
                     icone: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/solutto-logo.png",
                     link: "https://solutto.com.br/"
                 },
                 {
-                    imagem: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/mokaly-thumb.png",
+                    imagem: "https://kineticsolutions.s3.sa-east-1.amazonaws.com/mokaly-thumb_otimizada.webp",
                     nome: "Mokaly",
                     icone: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/mokaly-logo.png",
                     link: "https://mokaly.com/"
                 },
                 {
-                    imagem: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/sistema-mokaly-thumb.png",
+                    imagem: "https://kineticsolutions.s3.sa-east-1.amazonaws.com/sistema-mokaly-thumb_otimizada.webp",
                     nome: "Mokaly - Sistema",
                     icone: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/mokaly-logo.png",
                     link: "https://mokaly.com/"
                 },
                 {
-                    imagem: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/cda-thumb.png",
+                    imagem: "https://kineticsolutions.s3.sa-east-1.amazonaws.com/cda-thumb_otimizada.webp",
                     nome: "CDA - Feito por KSI",
                     icone: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/ksi.png",
                     link: "https://kineticsolutions.com.br/"
                 },
                 {
-                    imagem: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/cademint-thumb.png",
+                    imagem: "https://kineticsolutions.s3.sa-east-1.amazonaws.com/cademint-thumb_otimizada.webp",
                     nome: "Cademint",
                     icone: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/cademint-logo.png",
                     link: "https://cademint.netlify.app/"
                 },
                 {
-                    imagem: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/cademint-system.png",
+                    imagem: "https://kineticsolutions.s3.sa-east-1.amazonaws.com/cademint-system_otimizada.webp",
                     nome: "Sistema Cademint",
                     icone: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/cademint-logo.png",
                     link: "https://app-cademint.netlify.app/"
                 },
                 {
-                    imagem: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/gourmetech.png",
+                    imagem: "https://kineticsolutions.s3.sa-east-1.amazonaws.com/gourmetech_otimizada.webp",
                     nome: "Gourmetech",
                     icone: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/logo-gourmetech.svg",
                     link: "https://dev-gourmetech.netlify.app/"
                 },
                 {
-                    imagem: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/yuume.png",
+                    imagem: "https://kineticsolutions.s3.sa-east-1.amazonaws.com/yuume_otimizada.webp",
                     nome: "Yuume",
                     icone: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/logo.png",
                     link: "https://yuume.netlify.app"
                 },
                 {
-                    imagem: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/ieq-farol-thumbs.png",
+                    imagem: "https://kineticsolutions.s3.sa-east-1.amazonaws.com/ieq-farol-thumbs_otimizada.webp",
                     nome: "IEQ - Farol",
                     icone: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/ieq-farol-logo.png",
                     link: "https://ieq-farol.netlify.app/"
                 },
                 {
-                    imagem: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/saymon-felipe-thumb.jpg",
+                    imagem: "https://kineticsolutions.s3.sa-east-1.amazonaws.com/saymon-felipe-thumb_otimizada.webp",
                     nome: "Saymon Felipe - Portfólio",
                     icone: "https://rabsystems-storage.s3.sa-east-1.amazonaws.com/ksi.png",
                     link: "https://saymon-felipe.netlify.app/"
